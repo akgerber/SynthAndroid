5cb3d826632290da063c77ee30f3d0ff4785599d
akgerber <akgerber@slab.wv.cc.cmu.edu>
2010-04-22 20:36:22 -0400

    UI essentially complete. Connects via Bluetooth.

--- src/edu/cmu/ece/sythesthesia/SynthHome.java
+++ src/edu/cmu/ece/sythesthesia/SynthHome.java
@@ -1,25 +1,48 @@
 package edu.cmu.ece.sythesthesia;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.UUID;
+
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemSelectedListener;
 
 public class SynthHome extends Activity {
-    /** Called when the activity is first created. */
 	private String TAG = "SynthHome";
 	
-	private BluetoothAdapter mBluetoothAdapter;
+    // Intent request codes
+    private static final int REQUEST_CONNECT_DEVICE = 1;
+    
+    // Bluetooth-related globals
+    private BluetoothAdapter bta;
+    private BluetoothDevice remotebt;
+    private BluetoothSocket bts;
+    private OutputStream os;
+    private OutputStreamWriter osw;
+    private InputStream is;
+    private InputStreamReader isr;
+    private UUID uuid = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); //magic
+    
+    //UI Components
+    private TextView status;
 	private Button sendButton;
 	private Spinner scaleSpinner;
 	
@@ -28,7 +51,7 @@ public class SynthHome extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        //Initialize scale spinner
+        //Initialize UI elements
         scaleSpinner = (Spinner) findViewById(R.id.scale_spinner);
         if (scaleSpinner != null) {
 	        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
@@ -38,32 +61,76 @@ public class SynthHome extends Activity {
         } else {
         	//TODO: error handling
         }
-        
-        //initialize button
         sendButton = (Button) findViewById(R.id.send_button);
         if (sendButton != null) {
         	sendButton.setOnClickListener(mSendButtonListener);
         } else {
         	//TODO error handling
         }
+        status = (TextView) findViewById(R.id.conn_status);
         
         //Initialize Bluetooth adapter
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (mBluetoothAdapter == null) {
+    	bta = BluetoothAdapter.getDefaultAdapter();
+		if (bta == null) {
         	Toast.makeText(this, R.string.bt_error, 3);
-        	
-        }
+		}
+
+
         
     }
     
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-    	return true;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
     }
     
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-    	return true;
+    	switch (item.getItemId()) {
+    	case R.id.scan:
+    		Intent getDevIntent = new Intent(this, DeviceListActivity.class);
+    		startActivityForResult(getDevIntent, REQUEST_CONNECT_DEVICE);
+    		break;
+    	case R.id.quit:
+    		quit();
+    		break;
+    	}
+    	return false;
+    }
+    
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	switch (requestCode) {
+    	//Getting a Bluetooth device
+    	case REQUEST_CONNECT_DEVICE:
+    		//Activity succeeded
+    		if (resultCode == Activity.RESULT_OK) {
+                // Get the device MAC address
+                String address = data.getExtras()
+                                     .getString(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+                // Get the BluetoothDevice object
+            	remotebt = bta.getRemoteDevice(address);
+            	//Attempt to connect to the device
+            	try {
+	                bts = remotebt.createRfcommSocketToServiceRecord(uuid);
+	                os = bts.getOutputStream();
+	                is = bts.getInputStream();
+	                bts.connect();
+            	} catch (Exception e) {
+        			Toast.makeText(this, R.string.bt_error, 5);            		
+            	}
+            	//Set status to success
+            	status.setText(R.string.connected);
+            //Activity failed
+    		} else {
+    			Toast.makeText(this, R.string.bt_error, 5);
+    		}		
+    	}
+    }
+    
+    private void quit() {
+    	System.exit(0);
     }
     
     private Button.OnClickListener mSendButtonListener = new Button.OnClickListener(){
